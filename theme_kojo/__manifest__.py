31e3f617edecf506e9f661d418e895d7ab64cfbe
Ajepe <ajepebabatope@gmail.com>
2019-10-26 16:49:38 +0100

    [IMP]

--- theme_kojo/__manifest__.py
+++ theme_kojo/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', "website", "theme_default"],
+    'depends': ['base', "website_sale", "theme_default"],
 
     # always loaded
     'data': [
